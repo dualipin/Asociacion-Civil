64e17d56e10920d873e41df7aa599771c1b6f715
SANREY <118947989+dualipin@users.noreply.github.com>
2023-01-16 00:32:38 -0600

    Adicion de Node. y uso de React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './style.css'
+
+export function App() {
+  return (
+    <nav class="navbar">
+      <a href="/" class="navbar-brand">Asociación Civil</a>
+      <ul class="nav">
+        <li class="nav-item"><a href="/" class="nav-link">Registro</a></li>
+        <li class="nav-item"><a href="reporte.html" class="nav-link">Reporte</a></li>
+      </ul>
+    </nav>
+  )
+}
+
